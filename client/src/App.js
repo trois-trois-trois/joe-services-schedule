b2c9e101c21021338a37f646e3ec106a037d69de
Joe Boulos <jab12288@gmail.com>
2018-12-21 12:55:12 -0800

    most recent update, rendering schedules component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
 import sampleData from './sample_data';
-import Schedule from './components/Schedule.jsx'
-//test
+import Schedule from './components/Schedule';
+// test
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: sampleData,
-            rams: sampleData[0]
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <Schedule
-                    teamData={this.state.data}
-                    rams={this.state.rams}
-                />
-            </div>
-        );
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: sampleData,
+      rams: sampleData[0],
     };
-};
+  }
+
+  render() {
+    return (
+      <div>
+        <Schedule
+          teamData={this.state.data}
+          rams={this.state.rams}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
