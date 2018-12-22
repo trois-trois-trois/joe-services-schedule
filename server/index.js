661ce9df3722d18783e5f08c486911629c724e65
Joe Boulos <jab12288@gmail.com>
2018-12-21 20:07:20 -0800

    working on server and db schema

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('../database/index.js');
+const scheduledb = require('../database/index.js');
 
 let app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
+
+
+
+app.get('/rams', (req, res) => {
+  scheduledb.find({}, (err, data) => {
+  })
+  .then(function(data) {
+    res.json(data);
+  })
+  .catch(function(err) {
+    console.err(err);
+  })
+})
+
+
 
 
 let port = 3000;
