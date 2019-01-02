7301f93bc590deeb0a3781fa74da122b22a026e7
Joe Boulos <jab12288@gmail.com>
2019-01-02 15:04:42 -0800

    updated port variable

--- server/index.js
+++ server/index.js
@@ -22,9 +22,10 @@ app.get('/espn/schedules', (req, res) => {
   })
 })
 
-const port = 3000;
+// const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(port, function() {
+app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
 
